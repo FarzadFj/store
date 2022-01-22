e01edf3db832f203f643794812111526045aea44
FarzadFj <farzadfjofficial@gmail.com>
2022-01-22 03:48:16 +0330

    'add_2_new_routes'

--- routes/api.php
+++ routes/api.php
@@ -26,21 +26,26 @@ Route::post('/register',[UserController::class,'register']);
 
 Route::post('/login',[UserController::class,'login']);
 
-Route::put('/dashboard/{id}',[UserController::class,'update_dashboard'])->middleware('User_Login_Check_Middleware');
+Route::put('/dashboard/{id}',[UserController::class,'update_dashboard'])->middleware('user_check');
 
 // Admin
 Route::post('/admin/login',[AdminController::class,'login']);
 
-Route::get('/admin/users',[AdminController::class,'show_users'])->middleware('Admin_Check_Middleware');
+Route::get('/admin/users',[AdminController::class,'show_users'])->middleware('admin_check');
 
-Route::get('/admin/users_pagination',[AdminController::class,'get_users'])->middleware('Admin_Check_Middleware');
+Route::get('/admin/users_pagination',[AdminController::class,'get_users'])->middleware('admin_check');
 
-Route::put('/admin/user_profile/{id}',[AdminController::class,'update_user_profile'])->middleware('Admin_Check_Middleware');
+Route::put('/admin/user_profile/{id}',[AdminController::class,'update_user_profile'])->middleware('admin_check');
 
 // Product
 Route::get('/admin/home',[ProductController::class,'show_products']);
 
 Route::post('/admin/add_product',[ProductController::class,'add_product']);
+
+Route::put('/admin/update_product',[ProductController::class,'update_product']);
+
+Route::delete('/admin/delete_product',[ProductController::class,'delete_product']);
+
 
 
 
