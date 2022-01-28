a595d0651f0892cbc453c07e39ce307d9dd248ce
FarzadFj <farzadfjofficial@gmail.com>
2022-01-28 15:29:05 +0330

    ezafe kardan comment

--- app/Http/Middleware/Admin_Check_Middleware.php
+++ app/Http/Middleware/Admin_Check_Middleware.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use App\Models\Admin;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class Admin_Check_Middleware
 {
@@ -17,13 +19,6 @@ class Admin_Check_Middleware
      */
     public function handle(Request $request, Closure $next)
     {
-        // $admin = Admin::where('phoneNumber', $request->phoneNumber)->first();
-
-        // if(empty ($admin))
-        // {
-        //     return redirect('/admin/login');
-        // }
-
         if (isset($_SESSION['phoneNumber']) && isset($_SESSION['password']))
         {
             $someone = Admin::where('phoneNumber', $_SESSION['phoneNumber'])->where('password', $_SESSION['password'])->first();
@@ -33,6 +28,23 @@ class Admin_Check_Middleware
                 return redirect('admin/login');
             }
         }
+        
+        // ravesh behtar::
+
+        // $someone_phoneNumber = $request->user()->phoneNumber;
+        // $someone_password = $request->user()->password;
+
+        // $someone = Admin::where('phoneNumber', $someone_phoneNumber)->where('password', $someone_password)->first();
+
+        // if(empty ($someone))
+        // {
+        //     // return redirect('admin/login');
+        //     return response()->json([
+        //         'massege' => 'phoneNumber or password is wrong',
+        //         'massege 2' => $someone_phoneNumber,
+        //         'massege 3' => $someone_password
+        //     ], 400);
+        // }
 
         return $next($request);
     }
