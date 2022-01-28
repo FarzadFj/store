7bc4dbd96f9dc403d53a4e20e4cfbfdf36b5d533
FarzadFj <farzadfjofficial@gmail.com>
2022-01-28 15:23:34 +0330

    ezafe kardan function jadid

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -17,7 +18,7 @@ class ProductController extends Controller
         ]);
     }
 
-    public function add_product()
+    public function add_product(Request $request)
     {
         $data = [];
         $data = $request->all();
@@ -25,7 +26,8 @@ class ProductController extends Controller
             'title' => 'required|max:100',
             'description' => 'required|max:1000',
             'price' => 'required|max:10',
-            'img_url' => 'required|image|mimes:jpg,png,jpeg|max:2048'
+            'img_url' => 'required|image|mimes:jpg,png,jpeg|max:2048',
+            'stoke' =>'required|max:10'
         ]);
 
         if ($validator->fails()) {
@@ -33,10 +35,10 @@ class ProductController extends Controller
         }
 
         $image = $request->file('img_url');
-        $imageDestinationPath = 'uploads/products_images';
-        $img_name = date("Y/m/d - H:i:s").".".$data['title'];
-        $image->move($imageDestinationPath, $img_name);
-        $data['img_url'] = $img_name;
+        $image_extension = $image->guessClientExtension();
+        $img_new_name = date("Y/m/d- H-i-s") . "-" . $request->title . "." . $image_extension;
+        $image->move(public_path('uploads/products_images'), $img_new_name);
+        $data['img_url'] = $img_new_name;
 
         $product = Product::create($data);
 
@@ -54,24 +56,25 @@ class ProductController extends Controller
             'title' => 'max:100',
             'description' => 'max:1000',
             'price' => 'max:10',
-            'img_url' => 'image|mimes:jpg,png,jpeg|max:2048'
+            'img_url' => 'image|mimes:jpg,png,jpeg|max:2048',
+            'stoke' =>'max:10'
         ]);
 
         if ($validator->fails()) {
             return response()->json($validator->errors()->first(), 422);
         }
-
         empty($request->title) ? : $data['title'] = $request->title;
         empty($request->description) ? : $data['description'] = $request->description;
         empty($request->price) ? : $data['price'] = $request->price;
         if(!empty($request->img_url))
         {
             $image = $request->file('img_url');
-            $imageDestinationPath = 'uploads/products_images';
-            $img_name = date("Y/m/d - H:i:s").".".rand(1111111111,9999999999);
-            $image->move($imageDestinationPath, $img_name);
-            $data['img_url'] = $img_name;
+            $image_extension = $image->guessClientExtension();
+            $img_new_name = date("Y/m/d- H-i-s").".".rand(1111111111,9999999999). ".".$image_extension;
+            $image->move(public_path('uploads/products_images'), $img_new_name);
+            $data['img_url'] = $img_new_name;
         }
+        empty($request->stoke) ? : $data['stoke'] = $request->stoke;
 
         Product::where('id',$id)->update($data);
 
@@ -98,5 +101,31 @@ class ProductController extends Controller
             'code' => 2,
             'message' =>'Product Not Found'
         ],404);
+    }
+
+    public function filter_product($data, Request $request)
+    {
+        $date = 'date';
+        $stoke = 'stoke';
+        $category = 'category';
+
+        if($data == $date)
+        {
+
+        }
+
+        if($data == $stoke)
+        {
+
+        }
+
+        if($data == $category)
+        {
+
+        }
+
+        return response()->json([
+            'message' => 'There Is No Such Filter'
+        ]);        
     }
 }
