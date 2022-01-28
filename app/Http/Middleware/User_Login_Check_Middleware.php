594aa314f0dbfd8bdc2583cc3cf4b7774bca59ab
FarzadFj <farzadfjofficial@gmail.com>
2022-01-28 15:29:40 +0330

    comment kardan khat ha

--- app/Http/Middleware/User_Login_Check_Middleware.php
+++ app/Http/Middleware/User_Login_Check_Middleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class User_Login_Check_Middleware
 {
@@ -18,20 +19,37 @@ class User_Login_Check_Middleware
     public function handle(Request $request, Closure $next)
     {
 
-        if (isset($_SESSION['phoneNumber']) && isset($_SESSION['password']))
-        {
-            $someone = User::where('phoneNumber', $_SESSION['phoneNumber'])->where('password', $_SESSION['password'])->first();
+        // if (isset($_SESSION['phoneNumber']) && isset($_SESSION['password']))
+        // {
+        //     $someone = User::where('phoneNumber', $_SESSION['phoneNumber'])->where('password', $_SESSION['password'])->first();
 
-            if(empty ($someone))
-            {
-                return redirect('login');
-            }
-        }
+        //     if(empty ($someone))
+        //     {
+        //         return redirect('login');
+        //     }
+        // }
 
         // if(!Auth::check())
         // {
         //     return redirect('login');
         // }
+
+        // $someone_phoneNumber = $request->user()->phoneNumber;
+        // $someone_password = $request->user()->password;
+
+        // $someone = User::where('phoneNumber', $someone_phoneNumber)->where('password', $someone_password)->first();
+
+        // if(empty ($someone))
+        // {
+        //     // return redirect('admin/login');
+        //     return response()->json([
+        //         'massege' => 'phoneNumber or password is wrong',
+        //         'massege 2' => $someone_phoneNumber,
+        //         'massege 3' => $someone_password
+        //     ], 400);
+        // }
+
+        
 
         return $next($request);
     }
