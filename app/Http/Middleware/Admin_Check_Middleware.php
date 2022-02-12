96d2d159e5ec95c1061f0c2320111671336c6b43
FarzadFj <farzadfjofficial@gmail.com>
2022-02-12 15:19:41 +0330

    update commenting on codes

--- app/Http/Middleware/Admin_Check_Middleware.php
+++ app/Http/Middleware/Admin_Check_Middleware.php
@@ -19,32 +19,35 @@ class Admin_Check_Middleware
      */
     public function handle(Request $request, Closure $next)
     {
-        if (isset($_SESSION['phoneNumber']) && isset($_SESSION['password']))
-        {
-            $someone = Admin::where('phoneNumber', $_SESSION['phoneNumber'])->where('password', $_SESSION['password'])->first();
+        // if (isset($_SESSION['phoneNumber']) && isset($_SESSION['password']))
+        // {
+        //     $someone = Admin::where('phoneNumber', $_SESSION['phoneNumber'])->where('password', $_SESSION['password'])->first();
 
-            if(empty ($someone))
-            {
-                return redirect('admin/login');
-            }
-        }
+        //     if(empty ($someone))
+        //     {
+        //         return redirect('admin/login');
+        //     }
+        // }
         
         // ravesh behtar::
+        
+        /*
+        $someone_phoneNumber = $request->user()->phoneNumber;
+        $someone_password = $request->user()->password;
 
-        // $someone_phoneNumber = $request->user()->phoneNumber;
-        // $someone_password = $request->user()->password;
+        $someone = Admin::where('phoneNumber', $someone_phoneNumber)->where('password', $someone_password)->first();
 
-        // $someone = Admin::where('phoneNumber', $someone_phoneNumber)->where('password', $someone_password)->first();
+        if(empty ($someone))
+        {
+            // return redirect('admin/login');
+            return response()->json([
+                'massege' => 'phoneNumber or password is wrong',
+                'massege 2' => $someone_phoneNumber,
+                'massege 3' => $someone_password
+            ], 400);
+        }
+        */
 
-        // if(empty ($someone))
-        // {
-        //     // return redirect('admin/login');
-        //     return response()->json([
-        //         'massege' => 'phoneNumber or password is wrong',
-        //         'massege 2' => $someone_phoneNumber,
-        //         'massege 3' => $someone_password
-        //     ], 400);
-        // }
 
         return $next($request);
     }
